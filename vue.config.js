ac1bc1b8dec2825a53db4c1dea636b1aa8f8beac
DIGITRONICAIT\giovanni.venturelli <giovanni.venturelli@digitronica.it>
2020-10-01 12:27:59 +0200

    attempt to include vuetify-loader

---
+++ vue.config.js
@@ -0,0 +1,51 @@
+
+function enableShadowCss(config) {
+  const configs = [
+    config.module.rule('vue').use('vue-loader'),
+    config.module.rule('css').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('css').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('css').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('css').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('postcss').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('postcss').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('postcss').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('postcss').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('scss').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('scss').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('scss').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('scss').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('sass').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('sass').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('sass').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('sass').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('less').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('less').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('less').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('less').oneOf('normal').use('vue-style-loader'),
+    config.module.rule('stylus').oneOf('vue-modules').use('vue-style-loader'),
+    config.module.rule('stylus').oneOf('vue').use('vue-style-loader'),
+    config.module.rule('stylus').oneOf('normal-modules').use('vue-style-loader'),
+    config.module.rule('stylus').oneOf('normal').use('vue-style-loader'),
+  ];
+  configs.forEach(c => c.tap(options => {
+    options.shadowMode = true;
+    return options;
+  }));
+}
+
+
+
+
+
+module.exports = {
+  chainWebpack: config => {
+    enableShadowCss(config);
+    config.plugin('VuetifyLoaderPlugin').tap(args => [{
+      match (originalTag, { kebabTag, camelTag, path, component }) {
+        if (kebabTag.startsWith('core-')) {
+          return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
+        }
+      }
+    }])
+  }
+}
